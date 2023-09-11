71ab6e58842db8c0fe27b65e7c6fb5b1ffd4a3c0
unknown <arigbedeomobolaji@gmail.com>
2023-09-11 10:17:28 +0100

    update prod.js

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 export default {
@@ -6,5 +6,4 @@ export default {
 	PAYPAL_CLIENTID: process.env.PAYPAL_CLIENTID,
 	PAYSTACK_CLIENTID: process.env.PAYSTACK_CLIENTID,
 	MONGO_URI: process.env.MONGO_URI,
-	PORT: process.env.PORT,
 };
