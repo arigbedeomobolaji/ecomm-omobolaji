ef7449d6e40cd4edd4553bc70c5ad7cc2e5a84ea
unknown <arigbedeomobolaji@gmail.com>
2023-09-11 11:04:08 +0100

    update server.jsv2

--- backend/server.js
+++ backend/server.js
@@ -51,11 +51,18 @@ app.use("/api/orders", orderRouter);
 app.use("/api/upload", uploadRouter);
 
 // client data
+// if (process.env.NODE_ENV === "production") {
+// 	// set static folder
+// 	app.use(express.static("client/build"));
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// 	});
+// }
+
 if (process.env.NODE_ENV === "production") {
-	// set static folder
-	app.use(express.static("client/build"));
+	app.use("/", express.static("../client/build"));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+		res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
 	});
 }
 
