b7e997106b1f06c18a3d26f2719ed7d0facca533
unknown <arigbedeomobolaji@gmail.com>
2023-09-11 10:47:47 +0100

    update setupproxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
 	app.use(
 		"/api",
 		createProxyMiddleware({
-			target: "http://localhost:5000",
+			target: "https://ecomm-omobolaji-backend.vercel.app",
 			changeOrigin: true,
 		})
 	);
